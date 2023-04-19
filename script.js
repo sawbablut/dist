87cf4c34c18a19c6d75e0f2b03ae7048db5f98cd
Saw Ba Blut <sbb.zotefamily@gmail.com>
2023-04-19 16:15:28 +0630

    test

--- script.js
+++ script.js
@@ -4,28 +4,37 @@ const carouselItems = document.querySelectorAll('.carousel__item');
 const elems = Array.from(carouselItems);
 
 carouselList.addEventListener('click', function (event) {
+ 
   var newActive = event.target;
+  
   var isItem = newActive.closest('.carousel__item');
-
+  console.log(isItem);
   if (!isItem || newActive.classList.contains('carousel__item_active')) {
     return;
   };
-  
-  update(newActive);
+  update(isItem);
 });
+
+
 
 const update = function(newActive) {
   const newActivePos = newActive.dataset.pos;
 
-  const current = elems.find((elem) => elem.dataset.pos == 0);
-  const prev = elems.find((elem) => elem.dataset.pos == -1);
-  const next = elems.find((elem) => elem.dataset.pos == 1);
-  const first = elems.find((elem) => elem.dataset.pos == -2);
-  const last = elems.find((elem) => elem.dataset.pos == 2);
+  const current = elems.find((elem) => elem.dataset.pos == 3);
+  // const prev = elems.find((elem) => elem.dataset.pos == -1);
+  // const next = elems.find((elem) => elem.dataset.pos == 1);
+  // const first = elems.find((elem) => elem.dataset.pos == -2);
+  // const last = elems.find((elem) => elem.dataset.pos == 2);
+  // const three = elems.find((elem) => elem.dataset.pos == 3);
   
   current.classList.remove('carousel__item_active');
   
-  [current, prev, next, first, last].forEach(item => {
+  // [current, prev, next, first, last, three].forEach(item => {
+  //   var itemPos = item.dataset.pos;
+
+  //   item.dataset.pos = getPos(itemPos, newActivePos)
+  // });
+  elems.forEach(item => {
     var itemPos = item.dataset.pos;
 
     item.dataset.pos = getPos(itemPos, newActivePos)
@@ -33,11 +42,22 @@ const update = function(newActive) {
 };
 
 const getPos = function (current, active) {
-  const diff = current - active;
+  console.log(current, active);
+  var diff = current - active + 3;
 
-  if (Math.abs(current - active) > 2) {
-    return -current
+  // if (Math.abs(current - active) > 2) {
+  //   return -current
+  // }
+
+  if (diff <= 0) {
+    diff = diff + 10;
   }
+
+  if (diff > 10) {
+    diff = diff - 10;
+  }
+
+  // const diff = current
 
   return diff;
 }
